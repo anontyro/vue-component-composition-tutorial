3c096933c19b4839c3b9bba8cbcb5dfe1d56d446
Alexander Wilkinson <alexwilkinson@gmail.com>
2021-05-23 12:55:35 +0800

    added in basic container next to explore more buttons and then containers

---
+++ src/components/containers/BaseContainer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-container">
+    <div class="header-section">
+      <slot name="header">Header</slot>
+    </div>
+    <div class="main-body">
+      <slot>Body is default</slot>
+    </div>
+    <div class="footer-section">
+      <slot name="footer">footer section</slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+export default defineComponent({
+  name: 'BaseContainer',
+});
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  height: 800px;
+  margin: 10px auto;
+  border: 1px solid #d9d9d9;
+  border-radius: 5px;
+}
+
+.header-section {
+  border-bottom: 1px solid #d9d9d9;
+}
+
+.footer-section {
+  border-top: 1px solid #d9d9d9;
+}
+
+.main-body {
+  display: flex;
+  flex-grow: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
+    <BaseContainer>
+      <p>The main content will go in here and it will keep going and going</p>
+      <p>With more here</p>
+    </BaseContainer>
     <BaseButton to="/about"> </BaseButton>
   </div>
 </template>
@@ -7,11 +11,13 @@
 <script lang="ts">
 import {defineComponent} from 'vue';
 import BaseButton from '@/components/buttons/BaseButton.vue';
+import BaseContainer from '@/components/containers/BaseContainer.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     BaseButton,
+    BaseContainer,
   },
 });
 </script>
