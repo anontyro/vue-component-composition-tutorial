4841903c23e5c443b943e472d8097df46e7d6735
Alexander Wilkinson <alexwilkinson@gmail.com>
2021-05-24 22:22:24 +0800

    added new container items

---
+++ src/components/containers/ImageContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <BaseContainer>
+    <img
+      :class="`img-item ${classOverride}`"
+      :alt="imgAlt"
+      :src="require(`@/assets/imgs/${imgName}`)"
+    />
+  </BaseContainer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import BaseContainer from './BaseContainer.vue';
+
+export default defineComponent({
+  name: 'ImageContainer',
+  components: {
+    BaseContainer,
+  },
+  props: {
+    imgName: String,
+    imgAlt: {
+      type: String,
+      default: 'Some beautiful image most likely',
+    },
+    classOverride: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.img-item {
+  width: 100%;
+}
+</style>
---
+++ src/components/containers/InfoContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseContainer>
+    <template v-slot:header>
+      <h1>{{ title }}</h1>
+    </template>
+    <div class="text-content">
+      <slot></slot>
+    </div>
+  </BaseContainer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import BaseContainer from './BaseContainer.vue';
+
+export default defineComponent({
+  name: 'InfoContainer',
+  components: {
+    BaseContainer,
+  },
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.text-content {
+  overflow: auto;
+}
+</style>
